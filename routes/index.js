7f131ae4c8b69ae8b988c6b99d2c39e0b713f512
mistyflip <mark@gogglehead.com>
2018-05-13 16:46:55 +0200

    updated version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'CI CD Support' });
+  res.render('index', { title: 'CI CD Support 0.1.8' });
 });
 
 module.exports = router;
