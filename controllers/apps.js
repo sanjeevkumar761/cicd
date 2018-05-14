42c67b6b1145fd6afad680e7f95b34b3eee06fb1
mistyflip <mark@gogglehead.com>
2018-05-14 10:11:19 +0200

    config: added enpoints

--- controllers/apps.js
+++ controllers/apps.js
@@ -1,14 +1,14 @@
 var cfNodejsClient = require("cf-nodejs-client");
 
-const CloudController = new (require("cf-nodejs-client")).CloudController(endpoint);
-const UsersUAA = new (require("cf-nodejs-client")).UsersUAA;
-const CloudApps = new (require("cf-nodejs-client")).Apps(endpoint);
-
 const endpoint = "https://api.cf.eu10.hana.ondemand.com";
 const un = "sanjeev_kumar@rcomext.com";
 const pw = "Hackathon8%2";
 const spaceid = "79a6cdb5-296b-46bd-b28a-a433492009de";
 const orgid = "53f33523-70ab-4c1a-b8ac-bfb233a0d853";
+
+const CloudController = new (require("cf-nodejs-client")).CloudController(endpoint);
+const UsersUAA = new (require("cf-nodejs-client")).UsersUAA;
+const CloudApps = new (require("cf-nodejs-client")).Apps(endpoint);
 
 // App functions
 function appinfo(req, res, next) {
@@ -30,6 +30,7 @@ function applist(req, res, next) {
         res.send(resultApps);
       } else {
         let appcount = resultApps.resources.length;
+        // Render the response using the PUG template and passing in any required values
         res.render('apps', {
           title: 'Listing ' + appcount + " Apps",
           subtitle: 'at ' + endpoint,
